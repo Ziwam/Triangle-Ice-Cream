d25ab9c99c7342a275ae07c618f8bfd92c0e76fe
Ziwa Mukungu <ziwabigboy@gmail.com>
2018-01-25 23:12:42 -0500

    added zomato api

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { Component } from 'react';
 import './App.css';
-import Map from './Map'
+import Map from './Map';
+import * as ZomatoAPI from './ZomatoAPI';
 import scriptloader from 'react-async-script-loader';
 
 class App extends Component {
+  state = {
+    list: []
+  }
+
+  componentDidMount() {
+    ZomatoAPI.getAll().then((list) => {
+      this.setState({list});
+    });
+  }
 
   render() {
     return (
       <div className="app">
-        <Map/>
       </div>
     )
   }
 }
 
-export default App
+export default App;
---
+++ src/ZomatoAPI.js
@@ -0,0 +1,32 @@
+//fetch results offset
+let start = 0;
+
+let results = [];
+
+const key = '18b4cfd2ff967ee9565522d7dc71f152';
+
+const headers = {
+	'Accept': 'application/json',
+	'user-key': key
+}
+
+export const getAll = () => 
+	fetch(`https://developers.zomato.com/api/v2.1/search?entity_id=898&entity_type=city&start=${start}&cuisines=233%2C501`, {
+		method: 'GET',
+		headers
+	})
+		.then(res => res.json())
+		.then(res => {
+
+			results.push(...res.restaurants);
+
+			//check to see if there are anymore results not grabbed
+			if((start+res.results_shown)<res.results_found){
+				start += res.results_shown;
+				//return nre fetch if not done
+				return getAll();
+			}else{
+				//return results if done
+				return results;
+			}
+		})
